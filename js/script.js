486f04833fc6b6eed49e3e5833443a3175b6be8e
thanhquangfuture <thanhquangfu@gmail.com>
2022-08-23 01:14:03 +0700

    1h23Aug

--- js/script.js
+++ js/script.js
@@ -136,7 +136,18 @@ $(document).ready(function() {
         },
       },
     });
-
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+      } else {
+        $('.go-top').fadeOut(200);
+      }
+    });
+    $('.go-top').click(function(event) {
+      event.preventDefault();
+      
+      $('html, body').animate({scrollTop: 0}, 300);
+    });
     
     
     
