9e652c749826f84f40a5c49872341cc243eb6c6a
thanhquangfuture <thanhquangfu@gmail.com>
2022-08-24 22:33:09 +0700

    22h24Aug

--- js/script.js
+++ js/script.js
@@ -167,6 +167,11 @@ $(document).ready(function() {
         });
       })
     });
+    $('a[href*="#"]').on('click', function(e) {
+      $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top,
+      }, 500, 'linear');
+  });
     
     
     
